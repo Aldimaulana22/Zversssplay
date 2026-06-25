8e04b81757c1cb67b04251a8f734ce51faf72a8c
Zversss <aldinurmaulana5@gmail.com>
2026-06-25 12:55:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const playBtn = document.getElementById("playBtn");
-
-let playing = false;
-
-playBtn.addEventListener("click", () => {
-    playing = !playing;
-
-    playBtn.textContent = playing ? "⏸" : "▶";
-});
